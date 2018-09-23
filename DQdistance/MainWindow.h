01c423cd5a9ca8d55f581f01d9be4870cd85e622
krzysiek06x <krzysiek06x@gmail.com>
2018-09-23 13:34:04 +0200

    dodanie wstepne

--- DQdistance/MainWindow.h
+++ DQdistance/MainWindow.h
@@ -34,6 +34,12 @@ namespace DQdistance {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	protected:
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Button^  button_generate;
 
 	private:
 		/// <summary>
@@ -48,11 +54,69 @@ namespace DQdistance {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MainWindow";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button_generate = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(257, 67);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(78, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Imiê i nazwisko";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(179, 101);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(156, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Liczba kilometrów do rozpisania";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(373, 59);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(200, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(373, 93);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(200, 20);
+			this->textBox2->TabIndex = 3;
+			// 
+			// button_generate
+			// 
+			this->button_generate->Location = System::Drawing::Point(235, 172);
+			this->button_generate->Name = L"button_generate";
+			this->button_generate->Size = System::Drawing::Size(305, 23);
+			this->button_generate->TabIndex = 4;
+			this->button_generate->Text = L"generuj";
+			this->button_generate->UseVisualStyleBackColor = true;
+			// 
+			// MainWindow
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(736, 390);
+			this->Controls->Add(this->button_generate);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MainWindow";
+			this->Text = L"MainWindow";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
 	};
