1de9bf94b73830170f076fd242894f1ab775c120
krzysiek06x <krzysiek06x@gmail.com>
2018-09-23 13:25:40 +0200

    add main window and configuration

---
+++ DQdistance/MainWindow.cpp
@@ -0,0 +1,13 @@
+#include "MainWindow.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	DQdistance::MainWindow MainWindow;
+	Application::Run(%MainWindow);
+}
